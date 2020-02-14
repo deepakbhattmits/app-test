e936b3a66cb27bc7735fb3fa35a66a46ed72493c
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-14 06:36:41 +0530

    refector

--- src/Root.js
+++ src/Root.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -5,11 +7,12 @@ import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(
-    reducers,
-    initialState,
-    applyMiddleware(reduxPromise)
-  );
+	console.log('root');
+	const store = createStore(
+		reducers,
+		initialState,
+		applyMiddleware(reduxPromise)
+	);
 
-  return <Provider store={store}>{children}</Provider>;
+	return <Provider store={store}>{children}</Provider>;
 };
