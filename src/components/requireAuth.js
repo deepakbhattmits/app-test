14a8218c49d129e072c4ade61af4dbe519dea0d0
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-14 06:35:27 +0530

    refector

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,32 +1,35 @@
+/** @format */
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export default ChildComponent => {
-  class ComposedComponent extends Component {
-    // Our component just got rendered
-    componentDidMount() {
-      this.shouldNavigateAway();
-    }
+	class ComposedComponent extends Component {
+		// Our component just got rendered
+		componentDidMount() {
+			this.shouldNavigateAway();
+		}
 
-    // Our component just got updated
-    componentDidUpdate() {
-      this.shouldNavigateAway();
-    }
+		// Our component just got updated
+		componentDidUpdate() {
+			this.shouldNavigateAway();
+		}
 
-    shouldNavigateAway() {
-      if (!this.props.auth) {
-        this.props.history.push('/');
-      }
-    }
+		shouldNavigateAway() {
+			if (!this.props.auth) {
+				this.props.history.push('/');
+			}
+		}
 
-    render() {
-      return <ChildComponent {...this.props} />;
-    }
-  }
+		render() {
+			console.log('TEST requireAuth');
+			return <ChildComponent {...this.props} />;
+		}
+	}
 
-  function mapStateToProps(state) {
-    return { auth: state.auth };
-  }
+	function mapStateToProps(state) {
+		return { auth: state.auth };
+	}
 
-  return connect(mapStateToProps)(ComposedComponent);
+	return connect(mapStateToProps)(ComposedComponent);
 };
