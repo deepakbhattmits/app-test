4e0fb01eaae27fd5d1113750d48f048a0e5a4d39
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-14 06:35:55 +0530

    refector

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,38 +1,41 @@
+/** @format */
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
 import requireAuth from 'components/requireAuth';
 
 class CommentBox extends Component {
-  state = { comment: '' };
+	state = { comment: '' };
 
-  handleChange = event => {
-    this.setState({ comment: event.target.value });
-  };
+	handleChange = event => {
+		this.setState({ comment: event.target.value });
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
+	handleSubmit = event => {
+		event.preventDefault();
 
-    this.props.saveComment(this.state.comment);
-    this.setState({ comment: '' });
-  };
+		this.props.saveComment(this.state.comment);
+		this.setState({ comment: '' });
+	};
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <h4>Add a Comment</h4>
-          <textarea onChange={this.handleChange} value={this.state.comment} />
-          <div>
-            <button>Submit Comment</button>
-          </div>
-        </form>
-        <button className="fetch-comments" onClick={this.props.fetchComments}>
-          Fetch Comments
-        </button>
-      </div>
-    );
-  }
+	render() {
+		console.log('TEST commentbox');
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<h4>Add a Comment</h4>
+					<textarea onChange={this.handleChange} value={this.state.comment} />
+					<div>
+						<button>Submit Comment</button>
+					</div>
+				</form>
+				<button className='fetch-comments' onClick={this.props.fetchComments}>
+					Fetch Comments
+				</button>
+			</div>
+		);
+	}
 }
 
 export default connect(null, actions)(requireAuth(CommentBox));
