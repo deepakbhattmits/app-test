3f1837eeb051d81918cf1035327ffd7bf31f0d6f
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-16 10:49:07 +0530

    refector

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,36 +1,38 @@
+/** @format */
+
 import React from 'react';
 import { mount } from 'enzyme';
 import moxios from 'moxios';
-import Root from 'Root';
-import App from 'components/App';
+import Root from '../Root';
+import App from '../components/App';
 
 beforeEach(() => {
-  moxios.install();
-  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
-    status: 200,
-    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
-  });
+	moxios.install();
+	moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+		status: 200,
+		response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+	});
 });
 
 afterEach(() => {
-  moxios.uninstall();
+	moxios.uninstall();
 });
 
 it('can fetch a list of comments and display them', done => {
-  const wrapped = mount(
-    <Root>
-      <App />
-    </Root>
-  );
+	const wrapped = mount(
+		<Root>
+			<App />
+		</Root>
+	);
 
-  wrapped.find('.fetch-comments').simulate('click');
+	wrapped.find('.fetch-comments').simulate('click');
 
-  moxios.wait(() => {
-    wrapped.update();
+	moxios.wait(() => {
+		wrapped.update();
 
-    expect(wrapped.find('li').length).toEqual(2);
+		expect(wrapped.find('li').length).toEqual(2);
 
-    done();
-    wrapped.unmount();
-  });
+		done();
+		wrapped.unmount();
+	});
 });
