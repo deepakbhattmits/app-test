ed049952ac59001859a684377392ef0d36ed4254
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-19 20:55:02 +0530

    refector

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -5,7 +5,7 @@ import { CHANGE_AUTH } from 'actions/types';
 export default function(state = false, action) {
 	switch (action.type) {
 		case CHANGE_AUTH:
-			console.log('TEST auth reducer');
+			// console.log('TEST auth reducer');
 			return action.payload;
 		default:
 			return state;
