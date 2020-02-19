43521c4f3276301a1710594a030c725bea30a43b
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-19 20:54:51 +0530

    refector

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
+/** @format */
+
 import { combineReducers } from 'redux';
 import commentsReducer from 'reducers/comments';
 import authReducer from 'reducers/auth';
+// combine reducer
 
 export default combineReducers({
-  comments: commentsReducer,
-  auth: authReducer
+	comments: commentsReducer,
+	auth: authReducer
 });
