ee719e7daf04622fb9c7c7af4cf0a167aca87cf0
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-20 07:04:42 +0530

    refector checkbox

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -25,7 +25,28 @@ it('creates one LI per comment', () => {
 });
 
 it('shows the text for each comment', () => {
-	console.log('test one ');
+	// console.log('test one ');
 	expect(wrapped.render().text()).toContain('Comment 1');
 	expect(wrapped.render().text()).toContain('Comment 2');
+});
+
+describe('select button ', () => {
+	it('has a checkbox with checked prop when click on button', () => {
+		wrapped.find('button.select').simulate('click', {
+			target: { checked: true }
+		});
+		wrapped.update();
+		let checkboxes = wrapped
+			.find('input[type="checkbox"]')
+			.map(el => expect(el.prop('checked')).toEqual(true));
+
+		console.log('TEST : ', checkboxes);
+		// wrapped.find('button.select').simulate('change', {
+		// 	target: { checked: false }
+		// });
+		// wrapped.update();
+		// wrapped
+		// 	.find('input[type="checkbox"]')
+		// 	.map(el => expect(el.prop('checked')).toEqual(false));
+	});
 });
