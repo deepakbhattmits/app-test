7624deffd2cc2bc7d2a049ff3610f622987ac0cd
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-20 07:04:48 +0530

    refector checkbox

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -4,18 +4,42 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class CommentList extends Component {
+	state = {
+		checked: false,
+		checkbox: [1, 2, 3, 4, 5]
+	};
 	renderComments() {
 		console.log('TEST COMEENTLIST');
 		return this.props.comments.map(comment => {
 			return <li key={comment}>{comment}</li>;
 		});
 	}
+	handleSelect = () => {
+		this.setState({ checked: !this.state.checked });
+	};
 
 	render() {
 		return (
 			<div>
 				<h4>Comment List</h4>
 				<ul>{this.renderComments()}</ul>
+
+				<button className='select' onClick={this.handleSelect}>
+					Select All
+				</button>
+				{this.state.checkbox.map(el => {
+					return (
+						<input
+							id={el}
+							key={el}
+							type='checkbox'
+							checked={this.state.checked}
+							onChange={() => {
+								this.setState({ checked: !this.state.checked });
+							}}
+						/>
+					);
+				})}
 			</div>
 		);
 	}
